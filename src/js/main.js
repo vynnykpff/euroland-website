6ffa89e8679a277a037e8e5d7d903d6d81f6f860
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-05-23 14:16:56 +0300

    add-home-page-mobile-version

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
+const navList = document.querySelector('.nav__list');
+
 document.querySelector('.burger').addEventListener('click', function () {
 	this.classList.toggle('active');
-	document.querySelector('.nav__list').classList.toggle('open');
+	navList.classList.toggle('open');
+
+	if (navList.classList[1] === 'open') {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = '';
+	}
 });
 
 // Footer update year
