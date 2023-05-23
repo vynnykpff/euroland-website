e8caff094f343ce9dd8738a390366ed914a4e550
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-05-24 01:51:07 +0300

    main-update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 const navList = document.querySelector('.nav__list');
+const burger = document.querySelector('.burger');
+const contacts = document.querySelector('.nav__item--contacts');
 
 document.querySelector('.burger').addEventListener('click', function () {
 	this.classList.toggle('active');
@@ -9,6 +11,12 @@ document.querySelector('.burger').addEventListener('click', function () {
 	} else {
 		document.body.style.overflow = '';
 	}
+});
+
+contacts.addEventListener('click', () => {
+	document.body.style.overflow = '';
+	navList.classList = 'nav__list';
+	burger.classList = 'burger';
 });
 
 // Footer update year
